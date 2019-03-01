49d164f0e3b3510d87797b160b7db320017243fa
Ristik Majumdar <34515568+1bl4z3r@users.noreply.github.com>
2019-03-01 22:58:38 +0530

    Add files via upload

---
+++ driver.js
@@ -0,0 +1,43 @@
+function redirect()
+{
+    var num = document.getElementById("url").value;
+    var numbers = /^[0-9]+$/;
+    if(num.match(numbers))
+    {
+        if((num.toString().length)>=6 && (num.toString().length)<=15)
+        {
+            if(document.getElementById("chkbx").checked == true)
+            {
+                var message = document.getElementById("msgbox").value; 
+            }
+            else
+            {
+                var message = '';
+            }
+            var wpurl = "https://api.whatsapp.com/send?phone=" + num + "&text=" + message;
+            if (window.confirm('Do you wish to be redirected?')) 
+                {
+                 window.open(wpurl,'_blank');
+                }
+            else
+                {
+                 location.reload();
+                }
+        }
+         else
+            {
+             alert('Wrong phone number format\nIt must be within 6-15 numbers');
+             location.reload();
+            }
+    }
+    else
+    {
+        alert('You need to type Phone Number');
+        location.reload();
+    }
+}
+function disen(status)
+{
+    status=!status;
+    document.getElementById("msgbox").disabled=status;
+}
