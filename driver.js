60c2f2c71ca254b76a135e2e05536543207369e5
Ristik Majumdar <34515568+1bl4z3r@users.noreply.github.com>
2019-04-02 22:27:14 +0530

    Update driver.js

--- driver.js
+++ driver.js
@@ -16,7 +16,7 @@ function redirect()
             }
             var wpurl = "https://api.whatsapp.com/send?phone=" + num + "&text=" + message;
             var showurl = "https://wa.me/" + num;
-            if (window.confirm('Redirect to: ' + showurl + '?')) 
+            if (window.confirm('Redirect to: ' + showurl + ' ?')) 
                 {
                  window.open(wpurl,'_blank');
                 }
